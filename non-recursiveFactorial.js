ad4a9622a08cdb4a383484485db91ba3bc396f9f
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-06-18 09:08:40 +0300

    non-recursive factorial

---
+++ non-recursiveFactorial.js
@@ -0,0 +1,13 @@
+
+
+// Non-recursive solution of the factorial
+function factorial(num) {
+  let total = 1;
+  for (let i = num; i > 1; i--) {
+    total *= i;
+  }
+  return total;
+}
+
+
+console.log(factorial(4));
