a52fc2026eb0dfb474b8662a07ab19e903c9ec5d
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-06-19 13:05:36 +0300

    binary search

--- Searching Algorithms/linearSearchPseudocode.js
+++ Searching Algorithms/linearSearchPseudocode.js
@@ -1,4 +1,13 @@
 
+/*
+• This function accepts an array and a value 
+• Loop through the array and check if the 
+current array element is equal to the value 
+• If it is, return the index at which the element 
+is found 
+• If the value is never found, return -1 
+*/
+
 function linearSearch(arr, num) {
     for (let i = 0; i < arr.length; i++){
         if(arr[i] === num){
